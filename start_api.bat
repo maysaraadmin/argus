fd71ada4e2684a54a9a93c242fe1c19e6a96963a
maysaraadmin <maysara.mubarak.mohamed@gmail.com>
2026-01-11 00:56:20 +0400

    up

--- start_api.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Argus MVP API...
-set PYTHONPATH=%~dp0src
-cd /d "%~dp0"
-python -m src.api.server
-pause
--- start_ui.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting Argus MVP UI...
-set PYTHONPATH=%~dp0src
-cd /d "%~dp0"
-streamlit run src/ui/app.py --server.address=0.0.0.0 --server.port=8501
-pause
